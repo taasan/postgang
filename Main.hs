f014625ff849c60531b959f99562704170a96ba2
Trond Aasan <trond.aasan@gmail.com>
2021-03-22 23:52:35 +0100

    Bruk GetOpt++

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main
@@ -15,7 +17,10 @@ import Control.Applicative
   , optional
   )
 import Control.Monad
-  ( replicateM
+  ( ap
+  , join
+  , replicateM
+  , when
   )
 
 import Data.Char
@@ -29,6 +34,7 @@ import Data.List
   )
 import Data.Maybe
   ( catMaybes
+  , fromJust
   , isNothing
   )
 import Data.String
@@ -46,13 +52,20 @@ import Numeric
   ( readHex
   )
 
+import System.Console.GetOpt
+  ( ArgDescr (..)
+  , ArgOrder (..)
+  , OptDescr (..)
+  , getOpt
+  , usageInfo
+  )
 import System.Environment
   ( getArgs
+  , getProgName
   )
 import System.IO
-  ( IOMode (WriteMode)
-  , hPutStr
-  , withFile
+  ( hPutStrLn
+  , stderr
   )
 import System.Process
   ( proc
@@ -62,15 +75,23 @@ import System.Process
 import Data.Bits
   ( shiftL
   )
+import Data.Function
+  ( (&)
+  )
 import Data.Functor
   ( void
   )
 import Data.Ix
   ( inRange
   )
+import System.Exit
+  ( exitFailure
+  , exitSuccess
+  )
 import Text.ParserCombinators.ReadP
   ( ReadP
   , char
+  , choice
   , get
   , munch1
   , readP_to_S
@@ -80,7 +101,9 @@ import Text.ParserCombinators.ReadP
   , string
   )
 import Text.Printf
-  ( printf
+  ( PrintfType
+  , hPrintf
+  , printf
   )
 import qualified Text.Show as Show
 
@@ -95,6 +118,13 @@ data DayOfWeek
   | Thursday
   | Friday
   deriving (Show, Eq, Enum)
+
+showDayName :: IsString p => DayOfWeek -> p
+showDayName Monday    = "mandag"
+showDayName Tuesday   = "tirsdag"
+showDayName Wednesday = "onsdag"
+showDayName Thursday  = "torsdag"
+showDayName Friday    = "fredag"
 
 data Month
   = January
@@ -139,8 +169,7 @@ skipUntil p = p <|> (get *> skipUntil p)
 
 entryP :: Parser Entry
 entryP = do
-  void
-    .  skipUntil
+  (void . skipUntil)
     $  string "\"nextDeliveryDays\""
     <* skipSpaces
     <* char ':'
@@ -152,8 +181,7 @@ jsonStringP :: Parser String
 jsonStringP = char '"' *> jString
  where
   jString = do
-    optFirst <- optional jsonChar
-    case optFirst of
+    optional jsonChar >>= \case
       Nothing                              -> "" <$ char '"'
       Just first | not (isSurrogate first) -> (first :) <$> jString
       Just first                           -> do
@@ -181,7 +209,7 @@ jsonStringP = char '"' *> jString
       <|> (string "\\u" *> escapeUnicode)
       <|> satisfy (\c -> not (c == '\"' || c == '\\' || isControl c))
    where
-    isControl c = c <= '\31'
+    isControl c = c < '\x20'
     escapeUnicode =
       chr . fst . head . readHex <$> replicateM 4 (satisfy isHexDigit)
 
@@ -191,13 +219,8 @@ deliveryDayP = DeliveryDay <$>- weekDayP <*>- (read <$> digitsP) <*>- monthP
 digitsP :: Parser String
 digitsP = munch1 isDigit
 
-weekDayP :: Parser DayOfWeek
-weekDayP = do
-  (Monday ¤ "mandag")
-    <|> (Tuesday ¤ "tirsdag")
-    <|> (Wednesday ¤ "onsdag")
-    <|> (Thursday ¤ "torsdag")
-    <|> (Friday ¤ "fredag")
+weekDayP :: ReadP DayOfWeek
+weekDayP = choice $ ap (¤) showDayName <$> [Monday .. Friday]
 
 monthP :: Parser Month
 monthP =
@@ -219,30 +242,80 @@ runParser parser str = case readP_to_S parser str of
   []             -> Nothing
   ((res, _) : _) -> Just res
 
+f :: Alternative f => (t -> Maybe a) -> t -> f a
+f g x = case g x of
+  Just res -> pure res
+  _        -> empty
+
+-- Options
+data Options = Options
+    { optOutput :: Maybe FilePath
+    , optReadStdin :: Bool
+    , optShowHelp :: Bool
+    } deriving Show
+
+defaultOptions :: Options
+defaultOptions =
+  Options { optOutput = Nothing, optReadStdin = False, optShowHelp = False }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['o']
+           ["output"]
+           (ReqArg (\val opts -> opts { optOutput = Just val }) "FILE")
+           "Path of output file"
+  , Option ['s']
+           ["stdin"]
+           (NoArg (\opts -> opts { optReadStdin = True }))
+           "Read input from stdin instead of fetching from posten.no"
+  , Option ['h']
+           ["help"]
+           (NoArg (\opts -> opts { optShowHelp = True }))
+           "Show this help message"
+  ]
+
 main :: IO ()
 main = do
-  now   <- getCurrentTime
-  str   <- fetchData
-  entry <- case runParser entryP str of
-    Just value -> pure value
-    _          -> error $ "Invalid data: " <> str
-  argv <- getArgs
-  let output = ical now entry
-  if any isNothing output
-    then error $ "Invalid input" <> show entry <> "\n" <> show output
-    else do
-      let fileName = case argv of
-            ("-" : _) -> "/dev/stdout"
-            (x   : _) -> x
-            _         -> "/dev/stdout"
-      withFile fileName WriteMode $ \h -> do
-        (hPutStr h . intercalate "\r\n") $ catMaybes output
+  prog <- getProgName
+  (Options outFile readFromStdin showHelp, rest) <- opts
+  when
+    (rest /= [])
+    do
+      hPrintf stderr "Superfluous arguments: %s" (show rest :: String)
+      hPutStrLn stderr ""
+      hPutStrLn stderr $ showUsage prog
+      exitFailure
+
+  when
+    showHelp
+    do
+      showUsage prog
+      exitSuccess
+
+  inData      <- if readFromStdin then getContents else fetchData
+  outputLines <- liftA2 ical getCurrentTime (f (runParser entryP) inData)
+
+  when (any isNothing outputLines)
+    $  error
+    $  "Invalid input"
+    <> "\n"
+    <> show outputLines
+
+  let icalOutput = intercalate "\r\n" $ catMaybes outputLines
+
+  if isNothing outFile || outFile == Just "-"
+    then putStr icalOutput
+    else writeFile (fromJust outFile) icalOutput
  where
+  showUsage :: PrintfType r => String -> r
+  showUsage = printf "Usage: %s" . flip usageInfo options
+
   ical :: UTCTime -> Entry -> [Maybe String]
   ical now (Entry days) =
     (Just <$> preamble)
       <> (days >>= event now . runParser deliveryDayP)
       <> (Just <$> ["END:VCALENDAR", ""])
+
   event :: UTCTime -> Maybe DeliveryDay -> [Maybe String]
   event now (Just dd@(DeliveryDay dayName d m)) =
     let
@@ -252,13 +325,14 @@ main = do
       Just
         <$> [ "BEGIN:VEVENT"
             , "UID:" <> show dd
-            , "SUMMARY:Posten kommer " <> show dayName
+            , "SUMMARY:Posten kommer " <> showDayName dayName
             , printf "DTSTART:%d%02d%02d" year (fromEnum m + 1) d
             , "DURATION:P1D"
             , "DTSTAMP:" <> formatTime defaultTimeLocale "%C%y%m%dT%H%M%S" now
             , "END:VEVENT"
             ]
   event _ _ = []
+
   preamble :: [String]
   preamble =
     [ "BEGIN:VCALENDAR"
@@ -281,3 +355,12 @@ main = do
       ]
     )
     ""
+
+  opts = do
+    argv <- getArgs
+    prog <- getProgName
+    let header = showUsage prog
+    case getOpt RequireOrder options argv of
+      (o, n, []) -> return (foldl (&) defaultOptions o, n)
+      (_, _, errs) ->
+        ioError (userError (join errs <> usageInfo header options))
